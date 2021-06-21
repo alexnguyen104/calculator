8e7b3b553133682fac24fb599c94816994a73671
alexnguyen104 <vstar.alex104@gmail.com>
2021-06-21 19:06:11 +0700

    js2

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const numbers = Array.from(document.getElementsByClassName("num"));
-const screenDown= document.getElementById("down");
-const screenUp= document.getElementById("up");
-let numDown = [];
-
+const operator = Array.from(document.getElementsByClassName("operator"));
+const screenDown1= document.getElementById("down");
+const screenDown2= document.getElementById("down");
+const screenUp = document.getElementById("up");
+const backspace = document.getElementById("backspace");
+const AC = document.getElementById("AC");
+let a =0;
+//number typing
 numbers.forEach(num => num.addEventListener('click', clickNumber));
 function clickNumber(){
   let numIndex = numbers.indexOf(this);
-  numDown.push(Number(numbers[numIndex].innerHTML))
-  screenDown.innerHTML=String(numDown);
-  console.log(screenDown.innerHTML)
+  screenDown1.value += numbers[numIndex].innerHTML;
+  
 }
+operator.forEach(operator => operator.addEventListener('click', operatorClick));
+function operatorClick(){
+  let opeIndex = operator.indexOf(this);
+  screenUp.value = Number(screenDown1.value) + operator[opeIndex].innerHTML;
+}
+backspace.onclick=function(){
+  screenDown.value = screenDown.value.substring(0,screenDown.value.length - 1);
+}
+AC.onclick=function(){
+  screenDown.value = "";
+  screenUp.value = "";
+}
+
+
 
