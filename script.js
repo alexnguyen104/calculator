201c1e32bf9e5da40ced968b134a69f8bf110688
alexnguyen104 <vstar.alex104@gmail.com>
2021-06-21 21:40:55 +0700

    complete calculator

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const numbers = Array.from(document.getElementsByClassName("num"));
 const operator = Array.from(document.getElementsByClassName("operator"));
+const equal = document.getElementById("equal").addEventListener('click', equalClick);
+const screenDown= document.getElementById("down");
 const screenDown1= document.getElementById("down");
-const screenDown2= document.getElementById("down");
 const screenUp = document.getElementById("up");
 const backspace = document.getElementById("backspace");
 const AC = document.getElementById("AC");
-let a =0;
 //number typing
 numbers.forEach(num => num.addEventListener('click', clickNumber));
 function clickNumber(){
   let numIndex = numbers.indexOf(this);
-  screenDown1.value += numbers[numIndex].innerHTML;
-  
+  screenDown.value += numbers[numIndex].innerText;
+  screenDown.value1 += numbers[numIndex].innerText;
 }
 operator.forEach(operator => operator.addEventListener('click', operatorClick));
 function operatorClick(){
   let opeIndex = operator.indexOf(this);
-  screenUp.value = Number(screenDown1.value) + operator[opeIndex].innerHTML;
+  if (operator[opeIndex].innerText == "x") {
+    operator[opeIndex].innerText = "*"
+  }
+  screenUp.value = Number(screenDown.value) + operator[opeIndex].innerText;
+  screenDown.value ="";
+  operator[opeIndex].innerText = "x"
+}
+function equalClick(){
+  screenDown.value = eval(screenUp.value + screenDown.value); 
+  screenUp.value = "";
+  if(screenDown.value == "Infinity"){
+    screenDown.value="CUI BAP"
+  }
 }
 backspace.onclick=function(){
   screenDown.value = screenDown.value.substring(0,screenDown.value.length - 1);
